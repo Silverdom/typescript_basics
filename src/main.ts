fdd387f8d9310f72ee7a542804ba90ef3dca91e5
SohamE <soham.584sengupta@gmail.com>
2024-05-25 21:31:33 +0530

    fundamental types 1

--- src/main.ts
+++ src/main.ts
@@ -1,1 +1,23 @@
-console.log("hi");
+let a: number = 4;
+let b: string;
+b = "abc";
+// let b: Number = new Number(5);
+
+// Type inference
+let c = 5;
+
+// Literal type from inference - d is not a number here.
+const d = 10;
+
+// undefined and null means does not exist, hence type is any.
+let m = null;
+m = undefined;
+
+console.log(a);
+
+// Type array
+let n: number[];
+n = [1, 2, 3];
+
+// Literal type does not work on objects as they are passed by ref
+const o = [1, 2, 3];
